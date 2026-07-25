738532eb6d13579f527edf64724e706395480e36
Dragostin Ivanov <86302107+mThunderbird@users.noreply.github.com>
2026-07-25 12:27:14 +0300

    Add: string-encode-and-decode - submission-8

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-8.py
@@ -0,0 +1,50 @@
+import re
+
+# str_output = re.sub(regex_search_term, regex_replacement, str_input)
+
+
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        return "".join([ str(len(word)) + "#" + word for word in strs])
+    
+    def decode(self, s: str) -> List[str]:
+        if s == "":
+            return []
+        strs = []
+        rest = s
+        while True:
+            num, rest = rest.split("#", 1)
+            length = int(num)
+            strs.append(rest[:length])
+            rest = rest[length:]
+            if len(rest) == 0:
+                return strs
+
+
+
+
+        
+
+
+
+
+    # def encode(self, strs: List[str]) -> str:
+    #     escaped = [re.sub(r"e", r"ee", word) for word in strs] # escape the escape
+    #     escaped = [re.sub(r"d", r"ed", word) for word in escaped] # escape the delimiter
+    #     encoded = ""
+    #     for i in range(len(escaped)):
+    #         encoded += escaped[i] + "d"
+
+    #     return encoded
+
+    # def decode(self, s: str) -> List[str]:
+    #     print("Input: [" + s + "]")
+    #     decoded = s
+    #     decoded = re.split(r"(?<!e)d|(?<!eee)(?<=ee)d", decoded) # if you see `eed` or just `d` split
+    #     decoded = decoded[:-1]
+    #     decoded = [re.sub(r"ed", "d", word) for word in decoded] # revert d escape
+    #     decoded = [re.sub(r"ee", "e", word) for word in decoded] # revert e escape
+    #     return decoded
+                
+
